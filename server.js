cd6ee44a6498bec5ba6572e943c4b76520a3d7fa
Ammar Swied <ammarsw@outlook.com>
2022-06-08 22:27:36 +0300

    added database url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import handleAPI from "./controllers/imageAPI.js";
 const db= knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-closed-95160',
       user : 'postgres',
     //   port: 3001,
       password : '4747',
